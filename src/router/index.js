e6d6ed792792f2069319a81a183837b717a4c918
shervan <shervangh@gmail.com>
2022-05-03 01:46:47 +0430

    create path for views comps

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView'
+import PortfolioView from '../views/PortfolioView'
+import TestimonailView from '../views/TestimonailView'
+import NewsView from '../views/NewsView'
+import ContactView from '../views/ContactView'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/portfolio',
+    name: 'Portfolio',
+    component: PortfolioView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutView
+  },
+  {
+    path: '/testimonail',
+    name: 'Testimonail',
+    component: TestimonailView
+  },
+  {
+    path: '/news',
+    name: 'News',
+    component: NewsView
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: ContactView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
