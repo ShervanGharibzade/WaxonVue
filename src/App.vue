efc17534780a6c6a4562725ff4c00604e648f752
shervan <shervangh@gmail.com>
2022-05-03 01:52:57 +0430

    put router link for each comps

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <router-link to="/"></router-link>
+    <router-link to="/About"></router-link>
+    <router-link to="/portfolio"></router-link>
+    <router-link to="/testimonail"></router-link>
+    <router-link to="/news"></router-link>
+    <router-link to="/contact"></router-link>
+  <router-view/>
+</template>
+
+<script>
+
+export default{
+}
+</script>
